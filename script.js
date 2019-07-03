27baac3d9c9a3eb197b1c87ac9e30ec0f2fb9c83
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-07-03 11:54:55 +0200

    making hamburger meniu from scratch in css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("dropMeniu");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
+    if (x.style.display === "none") {
       x.style.display = "block";
+    } else {
+      x.style.display = "none";
     }
   }
